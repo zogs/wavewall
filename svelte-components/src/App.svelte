9e4956fbf4d1fc28ad5d0106f8d6998f4ed12634
Simon Guichard <guichardsim@gmail.com>
2022-02-16 20:23:32 +0100

    remove fixed spots

--- svelte-components/src/App.svelte
+++ svelte-components/src/App.svelte
@@ -3,6 +3,7 @@
 	import ButtonNavPanel from './components/ButtonNavPanel.svelte'
 	import ModalSpotsList from './components/ModalSpotsList.svelte';
 	import ToolbarBottom from './components/ToolbarBottom.svelte';
+	import SurfLoader from './components/SurfLoader.svelte';
   import Panel from './components/Panel.svelte';
   import {spots, page, nbPages, nbPerPage, nbSpots, modal, visualMode} from './stores/AppStore';
 	import { onMount } from 'svelte';
@@ -13,11 +14,11 @@
   $: console.log($nbSpots, $page, $nbPages, $nbPerPage, $spots);
 
 	onMount(async () => {
-		// get spots from storage
 
-    //let _spots = JSON.parse(window.localStorage.getItem('spots'));
+		// get spots from storage
+    let _spots = JSON.parse(window.localStorage.getItem('spots'));
     // temporary hardcoding spots list
-    let _spots = [{"name":"Les Blancs Sablons","url":"https://www.yadusurf.com/meteo-surf-report/Les-Blancs-Sablons/1183"},{"name":"Le Petit Minou","url":"https://www.yadusurf.com/meteo-surf-report/Le-Petit-Minou/1187"},{"name":"La Torche","url":"https://www.yadusurf.com/meteo-surf-report/La-Torche/1203"},{"name":"Penfoul","url":"https://www.yadusurf.com/meteo-surf-report/Penfoul/1181"},{"name":"Pourville-sur-Mer","url":"https://www.yadusurf.com/meteo-surf-report/Pourville-sur-Mer/1148"}]
+    //let _spots = [{"name":"Les Blancs Sablons","url":"https://www.yadusurf.com/meteo-surf-report/Les-Blancs-Sablons/1183"},{"name":"Le Petit Minou","url":"https://www.yadusurf.com/meteo-surf-report/Le-Petit-Minou/1187"},{"name":"La Torche","url":"https://www.yadusurf.com/meteo-surf-report/La-Torche/1203"},{"name":"Penfoul","url":"https://www.yadusurf.com/meteo-surf-report/Penfoul/1181"},{"name":"Pourville-sur-Mer","url":"https://www.yadusurf.com/meteo-surf-report/Pourville-sur-Mer/1148"}]
 
 
 		if(!_spots) {
@@ -80,4 +81,5 @@
   :global(html) { height:100%;}
   :global(body) { height:100%;}
   main { height:100%; }
+  article { width:100%; height:100%; display:flex; align-items: center; justify-content: center; }
 </style>
